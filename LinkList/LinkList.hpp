329cc8d68b7f6dd50a4f31824ac3477ab43fc0f6
yourfriendyo <3395537299@qq.com>
2022-11-03 04:38:19 +0800

    linklist

--- LinkList/LinkList.hpp
+++ LinkList/LinkList.hpp
@@ -2,3 +2,24 @@
 #include <cassert>
 
 using namespace std;
+
+typedef int SLTDataType;
+
+typedef struct SListNode
+{
+    SLTDataType _data;
+    struct SListNode* _next;
+}SLTNode;
+
+
+SLTNode* BuySLTNode(SLTDataType x)
+{
+    SLTNode* new_node = (SLTNode*)malloc(sizeof(SLTNode));
+    if (new_node == nullptr)
+        exit(1);
+
+    new_node->_data = x;
+    new_node->_next = nullptr;
+
+    return new_node;
+}
--- LinkList/Test.cc
+++ LinkList/Test.cc
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+void TestSList1()
+{
+    SLTNode* n1 = BuySLTNode(1);
+    SLTNode* n2 = BuySLTNode(2);
+    SLTNode* n3 = BuySLTNode(3);
+    SLTNode* n4 = BuySLTNode(4);
+
+    n1->_next = n2;
+    n2->_next = n3;
+    n3->_next = n4;
+    n4->_next = nullptr;
+
+}
+
+
 int main()
 {
 
