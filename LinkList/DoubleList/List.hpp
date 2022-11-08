d87d5db78ccfd75ccf32104437afd9a44c103f70
yourfriendyo <3395537299@qq.com>
2022-11-08 13:30:03 +0800

    list done

--- LinkList/DoubleList/List.hpp
+++ LinkList/DoubleList/List.hpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cassert>
 
+using namespace std;
+
 typedef int DLTDataType;
 
 typedef struct ListNode
@@ -9,4 +11,92 @@ typedef struct ListNode
     DLTDataType val;
     struct ListNode* prev;
     struct ListNode* next;
-}DLNode;
+}DLTNode;
+
+
+DLTNode* BuyDLTNode(DLTDataType x)
+{
+    DLTNode* node = (DLTNode*)malloc(sizeof(DLTNode));
+    if (node == nullptr)
+        exit(1);
+    node->val = x;
+    node->next = node->prev = nullptr;
+
+    return node;
+}
+
+DLTNode* DLTInit()
+{
+    DLTNode* head = BuyDLTNode(-1);
+    head->next = head;
+    head->prev = head;
+
+    return head;
+}
+
+void DLTPushBack(DLTNode* head, DLTDataType x)
+{
+    assert(head);
+
+    DLTNode* newNode = BuyDLTNode(x);
+    DLTNode* tail = head->prev;
+
+    head->prev = newNode;
+    newNode->next = head;
+
+    tail->next = newNode;
+    newNode->prev = tail;
+}
+
+void DLTPopBack(DLTNode* head)
+{
+    assert(head && head->prev != head);
+
+    DLTNode* tail = head->prev;
+    DLTNode* prevTail = tail->prev;
+
+    head->prev = prevTail;
+    prevTail->next = head;
+
+    free(tail);
+}
+
+void DLTPushFront(DLTNode* head, DLTDataType x)
+{
+    assert(head);
+
+    DLTNode* newNode = BuyDLTNode(x);
+    DLTNode* front = head->next;
+
+    newNode->prev = head;
+    head->next = newNode;
+
+    front->prev = newNode;
+    newNode->next = front;
+}
+
+void DLTPopFront(DLTNode* head)
+{
+    assert(head && head->next != head);
+    DLTNode* first = head->next;
+    DLTNode* second = first->next;
+
+    head->next = second;
+    second->prev = head;
+
+    free(first);
+}
+
+void DLTPrint(DLTNode* list)
+{
+    assert(list);
+    DLTNode* cur = list->next;
+
+    while (cur != list)
+    {
+        cout << cur->val << " ";
+        cur = cur->next;
+    }
+
+    cout << endl;
+}
--- LinkList/DoubleList/Test.cc
+++ LinkList/DoubleList/Test.cc
@@ -3,8 +3,50 @@
 
 using namespace std;
 
+void TestDList1()
+{
+    DLTNode* list = DLTInit();
+
+    DLTPushBack(list, 0);
+    DLTPrint(list);
+    DLTPushBack(list, 1);
+    DLTPrint(list);
+    DLTPushBack(list, 2);
+    DLTPrint(list);
+
+    DLTPopBack(list);
+    DLTPrint(list);
+    DLTPopBack(list);
+    DLTPrint(list);
+    DLTPopBack(list);
+    DLTPrint(list);
+}
+
+void TestDList2()
+{
+    DLTNode* list = DLTInit();
+
+    DLTPushFront(list, 0);
+    DLTPrint(list);
+    DLTPushFront(list, 1);
+    DLTPrint(list);
+    DLTPushFront(list, 2);
+    DLTPrint(list);
+
+    DLTPopFront(list);
+    DLTPrint(list);
+    DLTPopFront(list);
+    DLTPrint(list);
+    DLTPopFront(list);
+    DLTPrint(list);
+
+}
+
 int main()
 {
+    TestDList1();
+    cout << "----------------------------------------" << endl;
+    TestDList2();
 
     return 0;
 }
