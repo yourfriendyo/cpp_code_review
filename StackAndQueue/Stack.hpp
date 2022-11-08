8bdfb586a33f641a389ff3ec6dc81fa64fac2af0
yourfriendyo <3395537299@qq.com>
2022-11-08 17:32:11 +0800

    stack&queue

---
+++ StackAndQueue/Stack.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+#include <cassert>
+
+using namespace std;
---
+++ StackAndQueue/Test.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+void TestStack1()
+{
+
+}
+
+void TestStack2()
+{
+
+}
+
+
+void TestQueue1()
+{
+
+}
+
+void TestQueue2()
+{
+
+}
+
+int main()
+{
+    TestStack1();
+    TestStack2();
+
+    TestQueue1();
+    TestQueue2();
+
+    return 0;
+}
