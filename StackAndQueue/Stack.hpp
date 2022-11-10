710abc46f48c7f87bcd08fdb7eba842b53009988
yourfriendyo <3395537299@qq.com>
2022-11-10 22:53:19 +0800

    stack

--- StackAndQueue/Stack.hpp
+++ StackAndQueue/Stack.hpp
@@ -12,3 +12,63 @@ typedef struct Stack
     int top;
     int capacity;
 }ST;
+
+void StackInit(ST* st)
+{
+    assert(st);
+
+    st->data = (STDataType*)malloc(sizeof(STDataType) * 4);
+    assert(st->data);
+
+    st->top = 0;
+    st->capacity = 4;
+}
+
+void StackDestory(ST* st)
+{
+    assert(st);
+
+    free(st->data);
+    st->top = st->capacity = 0;
+}
+
+void CheckCapacity(ST* st)
+{
+    if (st->top == st->capacity)
+    {
+        int newCapacity = st->capacity == 0 ? 4 : st->capacity * 2;
+        st->data = (STDataType*)realloc(st->data, sizeof(STDataType) * newCapacity);
+        assert(st->data);
+
+        st->capacity = newCapacity;
+    }
+}
+
+void StackPush(ST* st, STDataType x)
+{
+    assert(st);
+    CheckCapacity(st);
+    st->data[st->top++] = x;
+}
+
+void StackPop(ST* st)
+{
+    assert(st && st->top > 0);
+    st->top--;
+}
+
+bool StackEmpty(ST* st)
+{
+    return st->top == 0;
+}
+
+int StackSize(ST* st)
+{
+    return st->top;
+}
+
+STDataType StackTop(ST* st)
+{
+    assert(st->top > 0);
+    return st->data[st->top - 1];
+}
--- StackAndQueue/Test.cc
+++ StackAndQueue/Test.cc
@@ -1,10 +1,44 @@
 #include <iostream>
 #include <cassert>
+#include "Stack.hpp"
+#include "Queue.hpp"
 
 using namespace std;
 void TestStack1()
 {
+    ST st;
 
+    StackInit(&st);
+
+    StackPush(&st, 1);
+    cout << StackTop(&st) << endl;
+    StackPush(&st, 2);
+    cout << StackTop(&st) << endl;
+    StackPush(&st, 3);
+    cout << StackTop(&st) << endl;
+    StackPush(&st, 4);
+    cout << StackTop(&st) << endl;
+    StackPush(&st, 5);
+    cout << StackTop(&st) << endl;
+
+    cout << "stack is Empty ? " << StackEmpty(&st) << endl;
+    cout << "stack size ? " << StackSize(&st) << endl;
+
+    StackPop(&st);
+    cout << StackTop(&st) << endl;
+    StackPop(&st);
+    cout << StackTop(&st) << endl;
+    StackPop(&st);
+    cout << StackTop(&st) << endl;
+    StackPop(&st);
+    cout << StackTop(&st) << endl;
+    StackPop(&st);
+    // cout << StackTop(&st) << endl; //Assert
+
+    cout << "stack is Empty ? " << StackEmpty(&st) << endl;
+    cout << "stack size ? " << StackSize(&st) << endl;
+
+    StackDestory(&st);
 }
 
 void TestStack2()
