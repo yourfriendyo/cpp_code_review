2a4538eef6f788e98b9b4e9e7e3cf6e3402fb9b3
yourfriendyo <3395537299@qq.com>
2022-11-15 20:44:09 +0800

    heap start

---
+++ Heap/Heap.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <cassert>
+#include <cstdlib>
---
+++ Heap/Test.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+void TestHeap()
+{
+
+}
+
+int main()
+{
+	cout << "heap" << endl;
+
+	TestHeap();
+
+	return 0;
+}
