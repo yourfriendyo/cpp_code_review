b71f80b4a33358dd2567ee850d15b9aec977222a
yourfriendyo <3395537299@qq.com>
2022-11-21 19:56:24 +0800

    test

--- Heap/Heap.hpp
+++ Heap/Heap.hpp
@@ -9,105 +9,136 @@ typedef int HPDataType;
 
 typedef struct Heap
 {
-	HPDataType* a;
-	int size;
-	int capacity;
+    HPDataType* a;
+    int size;
+    int capacity;
 }Heap;
 
 void HeapInit(Heap* hp)
 {
-	assert(hp);
+    assert(hp);
 
-	hp->a = nullptr;
-	hp->size = hp->capacity = 0;
+    hp->a = nullptr;
+    hp->size = hp->capacity = 0;
+}
+
+void HeapBuild(Heap* hp, HPDataType* a, int sz)
+{
+    // assert(hp);
+    // for (int i = 0; i < sz; i++)
+    // 	HeapPush(hp, a[i]);
+
+    for (int i = (sz - 2) / 2; i >= 0; i--)
+        AdjustDown(a, sz, i);
 }
 
 void HeapDestroy(Heap* hp)
 {
-	assert(hp);
+    assert(hp);
 
-	free(hp->a);
-	hp->a = nullptr;
-	hp->size = hp->capacity = 0;
+    free(hp->a);
+    hp->a = nullptr;
+    hp->size = hp->capacity = 0;
 }
 
 void AdjustUp(int* a, int child);
+void AdjustDown(int* a, int size, int parent);
 
 void HeapPush(Heap* hp, HPDataType x)
 {
-	if (hp->size == hp->capacity)
-	{
-		int newCapacity = hp->capacity == 0 ? 4 : hp->capacity * 2;
+    if (hp->size == hp->capacity)
+    {
+        int newCapacity = hp->capacity == 0 ? 4 : hp->capacity * 2;
 
-		hp->a = (HPDataType*)realloc(hp->a, newCapacity * sizeof(int));
-		assert(hp->a);
+        hp->a = (HPDataType*)realloc(hp->a, newCapacity * sizeof(int));
+        assert(hp->a);
 
-		hp->capacity = newCapacity;
-	}
+        hp->capacity = newCapacity;
+    }
 
-	hp->a[hp->size++] = x;
+    hp->a[hp->size++] = x;
 
-	AdjustUp(hp->a, hp->size - 1);
+    AdjustUp(hp->a, hp->size - 1);
+}
+
+void HeapPop(Heap* hp)
+{
+    assert(hp && hp->size);
+
+    hp->a[0] = hp->a[hp->size - 1];
+    hp->size--;
+
+    AdjustDown(hp->a, hp->size, 0);
 }
 
 void AdjustUp(int* a, int child)
 {
-	int parent = (child - 1) / 2;
+    int parent = (child - 1) / 2;
 
-	while (child > 0)
-	{
+    while (child > 0)
+    {
 #ifdef BIG_HEAP
-		if (a[child] > a[parent])
+        if (a[child] > a[parent])
 #else
-		if (a[child] < a[parent])
+        if (a[child] < a[parent])
 #endif
-			swap(a[child], a[parent]);
-		else
-			break;
+            swap(a[child], a[parent]);
+        else
+            break;
 
-		child = parent;
-		parent = (child - 1) / 2;
-	}
-}
-
-void AdjustDown(int* a, int size, int parent);
-
-void HeapPop(Heap* hp)
-{
-	assert(hp && hp->size);
-
-	hp->a[0] = hp->a[hp->size - 1];
-	hp->size--;
-
-	AdjustDown(hp->a, hp->size, 0);
+        child = parent;
+        parent = (child - 1) / 2;
+    }
 }
 
 void AdjustDown(int* a, int size, int parent)
 {
-	int child = parent * 2 + 1;
+    int child = parent * 2 + 1;
 
-	while (child < size)
-	{
-		if (child + 1 < size && a[child] < a[child + 1])
-			child = child + 1;
+    while (child < size)
+    {
+#ifdef BIG_HEAP
+        if (child + 1 < size && a[child] < a[child + 1])
+#else
+        if (child + 1 < size && a[child] > a[child + 1])
+#endif
+            child++;
 
-		if (a[child] > a[parent])
-			swap(a[parent], a[child]);
-		else
-			break;
+        if (a[child] > a[parent])
+            swap(a[parent], a[child]);
+        else
+            break;
 
-		parent = child;
-		child = parent * 2 + 1;
-	}
+        parent = child;
+        child = parent * 2 + 1;
+    }
 }
 
 void HeapPrint(Heap* hp)
 {
-	int i = 0;
+    assert(hp);
 
-	for (int i = 0; i < hp->size; i++)
-		cout << hp->a[i] << " ";
+    for (int i = 0; i < hp->size; i++)
+        cout << hp->a[i] << " ";
 
-	cout << endl;
+    cout << endl;
 
+}
+
+int HeapSize(Heap* hp)
+{
+    assert(hp);
+    return hp->size;
+}
+
+int HeapTop(Heap* hp)
+{
+    assert(hp && hp->size);
+    return hp->a[0];
+}
+
+bool HeapEmpty(Heap* hp)
+{
+    assert(hp);
+    return hp->size;
 }
--- Heap/Test.cc
+++ Heap/Test.cc
@@ -6,7 +6,59 @@
 using namespace std;
 
 
-int main()
+void TestHeap1()
+{
+    Heap hp;
+    HeapInit(&hp);
+
+    int arr[] = { 4,3,1,9,2,5,8,6,0,7 };
+    int sz = sizeof(arr) / sizeof(int);
+
+    for (int i = 0; i < sz; i++)
+    {
+        HeapPush(&hp, arr[i]);
+        HeapPrint(&hp);
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < sz; i++)
+    {
+        HeapPrint(&hp);
+        HeapPop(&hp);
+    }
+
+    HeapDestroy(&hp);
+}
+
+void TestTopK(int k)
+{
+    Heap hp;
+    HeapInit(&hp);
+
+    int arr[] = { 4,3,1,9,2,5,8,6,0,7 };
+    int sz = sizeof(arr) / sizeof(int);
+
+#ifdef BIG_HEAP
+    // 1. 选第k大的数，建大堆
+    for (int i = 0; i < sz; i++)
+        HeapPush(&hp, arr[i]);
+
+    while (k--)
+    {
+        cout << HeapTop(&hp) << endl;
+        HeapPop(&hp);
+    }
+
+#else
+    //
+
+#endif
+
+    HeapDestroy(&hp);
+}
+
+void TestHeapSort()
 {
     Heap hp;
     HeapInit(&hp);
@@ -17,9 +69,21 @@ int main()
     for (int i = 0; i < sz; i++)
         HeapPush(&hp, arr[i]);
 
-    HeapPrint(&hp);
+    for (int i = 0; i < sz; i++) {
+        cout << HeapTop(&hp) << " ";
+        HeapPop(&hp);
+    }
 
     HeapDestroy(&hp);
+}
+
+int main()
+{
+    // TestHeap1();
+
+    // TestTopK(2);
+
+    TestHeapSort();
 
     return 0;
 }
