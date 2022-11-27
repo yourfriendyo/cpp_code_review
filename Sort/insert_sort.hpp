4ff998472ae767d995cd542cf0067d3d79ca2430
yourfriendyo <3395537299@qq.com>
2022-11-28 04:47:51 +0800

    sort

---
+++ Sort/insert_sort.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+void insert_sort(int* a, int n)
+{
+    for (int i = 0; i < n - 1; i++) // end最后是n-1位置
+    {
+        int end = i;
+        int x = a[end + 1]; // end后一个元素作待插元素
+
+        while (end >= 0)
+        {
+            if (a[end] > x)
+                a[end + 1] = a[end];
+            else
+                break;
+
+            --end;
+        }
+
+        a[end + 1] = x;
+    }
+}
+
+void shell_sort(int* a , int n)
+{
+    int gap = n;
+    while (gap > 1) // gap不断减小分组预排多次
+    {
+        gap = gap / 3 + 1; // 保证最后一定gap=1执行最后一次直接插排
+
+        for (int j = 0; j < gap; j++)
+        {
+            for (int i = j; i < n - gap; i += gap)
+            {
+                int end = i;
+                int x = a[end + gap];
+
+                while (end >= 0)
+                {
+                    if (a[end] > x)
+                        a[end + gap] = a[end];
+                    else
+                        break;
+
+                    end -= gap;
+                }
+
+                a[end + gap] = x;
+            }
+        }
+    }
+}
+
+void shell_sort_simp(int* a, int n)
+{
+    int gap = n;
+    while (gap > 1)
+    {
+        gap = gap / 3 + 1;
+
+        for (int i = 0; i < n - gap; i++) // 多组同时排
+        {
+            int end = i;
+            int x = a[end + gap];
+
+            while (end >= 0)
+            {
+                if (a[end] > x)
+                    a[end + gap] = a[end];
+                else
+                    break;
+
+                end -= gap;
+            }
+
+            a[end + gap] = x;
+        }
+    }
+}
---
+++ Sort/test.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include "insert_sort.hpp"
+
+using namespace std;
+
+typedef void (*call_back) (int*, int);
+
+void print_arr(int* a, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
+
+void test_sort(call_back sort)
+{
+    int a[] = { 8,3,0,7,2,9,1,6,4,5 };
+    int n = sizeof(a) / sizeof(int);
+
+    print_arr(a, n);
+    sort(a, n);
+    print_arr(a, n);
+}
+
+int main()
+{
+    // test_sort(insert_sort);
+    test_sort(shell_sort);
+    test_sort(shell_sort_simp);
+
+
+    return 0;
+}
