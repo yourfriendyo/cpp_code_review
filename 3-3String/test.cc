669e86db6adc21540bc81b4d80b23e01d0e76dc5
youfriendyo <3395537299@qq.com>
2023-02-17 12:19:21 +0800

    模版初阶结束

---
+++ 3-3String/test.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// template<class T>
+// void Swap(T& x, T& y)
+// {
+//     T tmp = x;
+//     x     = y;
+//     y     = tmp;
+// }
+
+// template<class T>
+// T Add(const T& left, const T& right)
+// {
+//     return left + right;
+// }
+
+// int main()
+// {
+//     // int a = 0, b = 1;
+//     // swap(a, b);
+//     // double c = 1.11, d = 2.22;
+//     // swap(c, d);
+
+//     int a1 = 1, a2 = 2;
+//     double d1 = 1.1, d2 = 2.2;
+
+//     cout << Add(a1, a2) << endl;
+//     cout << Add(d1, d2) << endl;
+//     cout << Add(a1, (int)d1) << endl;
+//     cout << Add((double)a1, d1) << endl;
+
+//     cout << Add<int>(a1, d1) << endl;
+//     cout << Add<double>(a1, d1) << endl;
+
+//     return 0;
+// }
