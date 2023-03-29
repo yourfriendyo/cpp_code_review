bd934e9b17dc2ff77d673b0f28691a030d20f950
yourfriendyo <3395537299@qq.com>
2023-03-30 00:13:07 +0800

    03-30 00:13

--- 4-3FileIO/myfile.c
+++ 4-3FileIO/myfile.c
@@ -1,15 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 
 int main()
 {
-    int fd = open("log.txt", O_CREAT | O_WRONLY, 0664);
+    umask(00);
+
+    // int fd = open("log.txt", O_CREAT | O_WRONLY, 0666);
+    int fd = open("log.txt", O_CREAT | O_WRONLY | O_TRUNC | O_RDONLY, 0666);
+    // int fd = open("log.txt", O_CREAT | O_WRONLY | O_APPEND, 0666);
+
+    if (fd < 0) 
+    {
+        printf("fd: %d, errno: %d, errdisc: %s\n", fd, errno, strerror(errno));
+        exit(1);
+    }
+
+    printf("fd: %d\n", fd);
+
+    // const char* msg = "hello 404 small ugly guy\n";
+    const char* msg = "aa";
+
+    for (int i = 0; i < 5; i++) 
+    {
+        /* char line[256];
+        snprintf(line, 256, "%d:%s", i, msg); */
+
+        ssize_t s = write(fd, msg, strlen(msg));
+    }
+
+    char buff[256];
+    ssize_t s = read(fd, buff, sizeof(buff) - 1);
+    printf("%s\n", buff);
+
 
     close(fd);
-
     return 0;
 }
 
